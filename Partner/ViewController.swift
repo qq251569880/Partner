ae4d8a4d91e3069ff2d536dc64adf88a7fce3225
hongtai zhang <qq251569880@gmail.com>
2015-12-06 14:28:22 +0800

    Initial Commit

---
+++ Partner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Partner
+//
+//  Created by 张宏台 on 15/12/6.
+//  Copyright © 2015年 张宏台. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
